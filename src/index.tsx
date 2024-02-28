1ba50d0567fde105b2b98975341d1fa187b9894a
Vlas Dvorovyi <vlas20421@gmail.com>
2024-02-28 14:06:17 +0200

    fix(#15): fix all Ci checks

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
 
 import App from './App';
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('root') as Element);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
